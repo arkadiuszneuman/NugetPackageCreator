7a3f6e9c4d59038f1ef997dea6b99ce90c6b7c2e
Arkadiusz Neuman <mrmoro@o2.pl>
2015-01-22 13:23:36 +0100

    View improvements

--- NugetCreator/Frm_MainForm.Designer.cs
+++ NugetCreator/Frm_MainForm.Designer.cs
@@ -41,24 +41,28 @@
             // 
             // simpleButton1
             // 
-            this.simpleButton1.Location = new System.Drawing.Point(87, 218);
+            this.simpleButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButton1.Location = new System.Drawing.Point(706, 70);
             this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.Size = new System.Drawing.Size(114, 23);
             this.simpleButton1.TabIndex = 0;
-            this.simpleButton1.Text = "simpleButton1";
+            this.simpleButton1.Text = "Wydaj wersję!";
             this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
             // 
             // memoEdit1
             // 
-            this.memoEdit1.Location = new System.Drawing.Point(24, 453);
+            this.memoEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.memoEdit1.Location = new System.Drawing.Point(12, 99);
             this.memoEdit1.Name = "memoEdit1";
-            this.memoEdit1.Size = new System.Drawing.Size(796, 160);
+            this.memoEdit1.Size = new System.Drawing.Size(808, 310);
             this.memoEdit1.TabIndex = 1;
             this.memoEdit1.UseOptimizedRendering = true;
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(65, 47);
+            this.labelControl1.Location = new System.Drawing.Point(12, 47);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(86, 13);
             this.labelControl1.TabIndex = 2;
@@ -66,42 +70,46 @@
             // 
             // frtxtOutputDirectory
             // 
+            this.frtxtOutputDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.frtxtOutputDirectory.EditValue = "\\\\192.168.255.21\\Wymiana\\ArekN\\NuGet";
-            this.frtxtOutputDirectory.Location = new System.Drawing.Point(157, 44);
+            this.frtxtOutputDirectory.Location = new System.Drawing.Point(108, 44);
             this.frtxtOutputDirectory.Name = "frtxtOutputDirectory";
-            this.frtxtOutputDirectory.Size = new System.Drawing.Size(413, 20);
+            this.frtxtOutputDirectory.Size = new System.Drawing.Size(712, 20);
             this.frtxtOutputDirectory.TabIndex = 3;
             // 
             // frtxtSlnPath
             // 
+            this.frtxtSlnPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.frtxtSlnPath.EditValue = "D:\\Praca\\inSolutions\\Projekty\\SzkieletAplikacji\\ApplicationFrame-Evaluation\\Appli" +
     "cationFrame.sln";
-            this.frtxtSlnPath.Location = new System.Drawing.Point(135, 9);
+            this.frtxtSlnPath.Location = new System.Drawing.Point(108, 9);
             this.frtxtSlnPath.Name = "frtxtSlnPath";
-            this.frtxtSlnPath.Size = new System.Drawing.Size(413, 20);
+            this.frtxtSlnPath.Size = new System.Drawing.Size(712, 20);
             this.frtxtSlnPath.TabIndex = 5;
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(43, 12);
+            this.labelControl2.Location = new System.Drawing.Point(12, 12);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(90, 13);
             this.labelControl2.TabIndex = 4;
             this.labelControl2.Text = "Solucja wejściowa:";
             // 
-            // Form1
+            // Frm_MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 634);
+            this.ClientSize = new System.Drawing.Size(832, 417);
             this.Controls.Add(this.frtxtSlnPath);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.frtxtOutputDirectory);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.memoEdit1);
             this.Controls.Add(this.simpleButton1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Frm_MainForm";
+            this.Text = "NuGet Generator";
             ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.frtxtOutputDirectory.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.frtxtSlnPath.Properties)).EndInit();
